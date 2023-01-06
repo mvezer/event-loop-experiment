cbbd905eb16e92aa5f5231f10be0324035ee107d
Matyas Vezer <matyas.vezer@bauerxcel.de>
2023-01-06 11:08:21 +0100

    making it simpler and easier to read

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Response, Request } from 'express'
 
 const ARRAY_SIZE = 100000000
+const PARTITION_SIZE = 10000
 const PORT = 42069
 
 const app = express()
@@ -9,16 +10,42 @@ const RANDOM_ARRAY = (new Array<number>(ARRAY_SIZE)).map(() => Math.round(Math.r
 
 const sleep = async (sleepMillisecs: number): Promise<void> => { return new Promise((resolve) => { setTimeout(resolve, sleepMillisecs) }) }
 
-export const processUnpartitioned = (array: Array<number>): void => {
-  for (let i = 0; i < array.length; i += 1) {
-    array[i] = array[i] * Math.round(Math.random() * 1000) / Math.round(Math.random() * 1000)
-  }
+const calculateStuff = (value: number): number => {
+  return value * Math.round(Math.random() * 1000) / Math.round(Math.random() * 1000)
 }
 
-export const processPartitioned = (array: Array<number>): void => {
-  for (let i = 0; i < array.length; i += 1) {
-    array[i] = array[i] * Math.round(Math.random() * 1000) / Math.round(Math.random() * 1000)
-  }
+const partitionedForEach = async <T>(array: Array<T>, callback: (param: T) => void, partitionSize = 1): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    let index = 0
+    const loop = () => {
+      const partitionStartIndex = index
+      const partitionEndIndex = Math.min(array.length, partitionStartIndex + partitionSize)
+      for (let partitionIndex = partitionStartIndex; partitionIndex < partitionEndIndex; partitionIndex += 1) {
+        try {
+          callback(array[partitionIndex])
+        } catch (err) {
+          reject(err)
+        }
+      }
+      index = partitionEndIndex
+      if (index < array.length) {
+        setImmediate(loop)
+      } else {
+        resolve()
+      }
+    }
+
+    loop()
+  })
+}
+
+const processUnpartitioned = async (array: Array<number>): Promise<void> => {
+  return new Promise((resolve) => {
+    for (let i = 0; i < array.length; i += 1) {
+      array[i] = calculateStuff(array[i])
+    }
+    resolve()
+  })
 }
 
 app.get('/', (_: Request, res: Response) => {
@@ -28,17 +55,21 @@ app.get('/', (_: Request, res: Response) => {
 app.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}`)
 
-  let doProcess
-  if (process.env.MODE === 'unpartitioned') {
-    console.log('Running UNPARTITIONED array processing...')
-    doProcess = processUnpartitioned
-  } else {
+  const isPartitioned = process.env.MODE === 'partitioned'
+  if (isPartitioned) {
     console.log('Running PARTITIONED array processing...')
-    doProcess = processUnpartitioned
+  } else {
+    console.log('Running UNPARTITIONED array processing...')
   }
 
   while (true) {
-    doProcess(RANDOM_ARRAY)
+    console.time('finished-a-batch')
+    if (isPartitioned) {
+      await partitionedForEach<number>(RANDOM_ARRAY, calculateStuff, PARTITION_SIZE)
+    } else {
+      processUnpartitioned(RANDOM_ARRAY)
+    }
+    console.timeEnd('finished-a-batch')
     await sleep(10)
   }
 })
